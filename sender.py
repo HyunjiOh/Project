07350e9694c81840593deeed95dd96898f892a3e
HyunjiOh <--applebeef25@gmail.com>
2018-05-18 15:26:43 +0900

    sending data to specific receiver

--- sender.py
+++ sender.py
@@ -3,4 +3,4 @@ import threading
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(('',0))
-sock.sendto("hello".encode(), ('115.145.242.44',10080))
+sock.sendto("hello".encode(), ('115.145.239.18',10080))
