4ad926b5df8e78a45b1aaaf1c4c9870589ebcb7e
HyunjiOh <--applebeef25@gmail.com>
2018-05-18 15:25:36 +0900

    continuously receiving data from sender and clarifying who's sending

--- receiver.py
+++ receiver.py
@@ -2,9 +2,9 @@ import socket
 import threading
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.bind(('115.145.242.44',10080))
+sock.bind(('115.145.239.18',10080))
 while True:
-    data, addr = sock.recvfrom(1472)
+    data, addr = sock.recvfrom(1024)
 
     print ('Server is received data : ', data.decode())
     print ('Send client ip : ', addr[0])
